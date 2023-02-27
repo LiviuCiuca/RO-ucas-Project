003a132a6f27d1bec4e1521f5cd2412967f331ac
liviu <liviu.ci88@gmail.com>
2023-02-27 18:22:40 +0000

    added first schetch of how my student page would contain, and a navbar component

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+//set 
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <ul id="nav">
+                    <li>Student</li>
+                    <li>Courses</li>
+                    <li>My Universities</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
---
+++ client/src/components/student/student.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { baseUrl } from '../urlEnv.env';
+
+const StudentById = () => {
+  const [student, setStudent] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  //the id i should get from auth
+  const getStudentById = async (id: number) => {
+    try {
+      const response = await axios.get(`${baseUrl}/student/${id}`);
+      setStudent(response.data);
+      setLoading(false);
+    } catch (error : any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const id = 1; 
+    getStudentById(id);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  //here i should display the student data
+  //gotta add the rest of the columns when i update main
+  const displayStudent= student.map(s => 
+    <div>
+      <p>s.name</p>
+      <p>s.email</p>
+      <p>s.age</p>
+    </div> )
+  return (
+    <div>
+      <h1>Student</h1>
+      <h2>{displayStudent}</h2>
+    </div>
+  );
+};
+
+export default StudentById;
